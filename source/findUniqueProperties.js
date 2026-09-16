8cf85f8e76ff36304dd5d16ed9fd7f2a76aee5e3
Juuwe <danya070207@gmail.com>
2026-09-16 20:32:33 +0300

    feat: add findUniqueProperties & add 3 new tests

---
+++ source/findUniqueProperties.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/**
+ * Проверяет, является ли значение валидным объектом
+ * @param {*} value - проверяемое значение
+ * @returns {boolean}
+ */
+const isValidObject = (value) =>
+  typeof value === "object" && value !== null && !Array.isArray(value);
+
+/**
+ * Функция, которая принимает два объекта и возвращает новый объект,
+ * содержащий только те свойства, которые уникальны для каждого из объектов
+ *
+ * @param {Object} firstObject - первый объект для сравнения
+ * @param {Object} secondObject - второй объект для сравнения
+ *
+ * @example
+ * * // returns {cat: 2, hamster: 4}
+ * const pets1 = {cat: 2, dog: 1, parrot: 3};
+ * const pets2 = {dog: 1, parrot: 3, hamster: 4};
+ * findUniqueProperties(pets1, pets2);
+ *
+ * @returns {Object} новый объект, содержащий только уникальные свойства
+ */
+
+function findUniqueProperties(firstObject, secondObject) {
+  if (!isValidObject(firstObject) || !isValidObject(secondObject)) {
+    throw new TypeError(
+      "Оба аргумента функции findUniqueProperties должны быть валидными объектами.",
+    );
+  }
+
+  const resultObject = {};
+
+  for (const key of Object.keys(firstObject)) {
+    if (!Object.hasOwn(secondObject, key)) {
+      resultObject[key] = firstObject[key];
+    }
+  }
+
+  for (const key of Object.keys(secondObject)) {
+    if (!Object.hasOwn(firstObject, key)) {
+      resultObject[key] = secondObject[key];
+    }
+  }
+
+  return resultObject;
+}
