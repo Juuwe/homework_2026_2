5acd5c11845df6d8476526b60be52e5f23cd0646
Juuwe <danya070207@gmail.com>
2026-09-18 00:01:57 +0300

    test: add new tests with primitives, set, map, array, null etc.

--- test/findUniqueProperties.js
+++ test/findUniqueProperties.js
@@ -51,4 +51,62 @@ QUnit.module('Тестируем функцию findUniqueProperties', function(
 
         assert.deepEqual(result, { a: 2, b: 1, c: 10, d: 20 }, 'Два объекта без общих ключей возвращают совокупность своих свойств.');
     });
+
+    QUnit.test('Работает правильно c объектами без прототипа', function(assert) {
+        const firstObject = Object.setPrototypeOf({ a: 1, b: 2 }, null);
+        const secondObject = Object.setPrototypeOf({ b: 20, c: 3 }, null);
+
+        const result = findUniqueProperties(firstObject, secondObject)
+
+        assert.deepEqual(result, { a: 1, c: 3 }, 'Два объекта без прототипа не меняют ожидаемое поведение функции.');
+    });
+
+    QUnit.test('Выбрасывает исключение, если вместо валидного объекта передан Set', function(assert) {
+        assert.throws(
+            () => findUniqueProperties(new Set([1, 5, 2]), { a: -7}),
+            TypeError,
+            'Передача Set в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если вместо валидного объекта передан массив', function(assert) {
+        assert.throws(
+            () => findUniqueProperties({ a: -7}, [1, 5, 2]),
+            TypeError,
+            'Передача массива в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если вместо валидного объекта передана строка', function(assert) {
+        assert.throws(
+            () => findUniqueProperties({ a: -7}, 'объект'),
+            TypeError,
+            'Передача string в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если вместо валидного объекта передано число', function(assert) {
+        assert.throws(
+            () => findUniqueProperties(123131, { a: -7}),
+            TypeError,
+            'Передача number в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если в параметры передан undefined', function(assert) {
+        assert.throws(
+            () => findUniqueProperties({ a: -7}, undefined),
+            TypeError,
+            'Передача undefined в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если в параметры передан boolean', function(assert) {
+        assert.throws(
+            () => findUniqueProperties({ a: -7}, false),
+            TypeError,
+            'Передача false в параметры выбрасывает TypeError')
+    });
+
+    QUnit.test('Выбрасывает исключение, если в параметры передан Map', function(assert) {
+        assert.throws(
+            () => findUniqueProperties({ a: -7}, new Map()),
+            TypeError,
+            'Передача false в параметры выбрасывает TypeError')
+    });
 });
